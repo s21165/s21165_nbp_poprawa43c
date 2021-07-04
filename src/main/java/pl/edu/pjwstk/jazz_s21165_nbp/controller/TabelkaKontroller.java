464f3af4c69a5e06d06f016965a24d28603fe94f
s21165 <s21165@pjwstk.edu.pl>
2021-07-04 21:02:07 +0200

    sexy look

--- src/main/java/pl/edu/pjwstk/jazz_s21165_nbp/controller/TabelkaKontroller.java
+++ src/main/java/pl/edu/pjwstk/jazz_s21165_nbp/controller/TabelkaKontroller.java
@@ -1,6 +1,7 @@
 package pl.edu.pjwstk.jazz_s21165_nbp.controller;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.pjwstk.jazz_s21165_nbp.entity.Tabelki;
@@ -27,9 +28,29 @@ public class TabelkaKontroller {
             notes = "This method will return average exchange rate in date range")
     @GetMapping("/{waluta}")
     public ResponseEntity<Tabelki> dajPieniadze(
-
+            @ApiParam(
+                    name = "waluta",
+                    type = "String",
+                    value = "wybierz walute",
+                    example = "EUR",
+                    required = true,
+                    defaultValue = "GBP")
             @PathVariable String waluta,
+            @ApiParam(
+                    name = "dataOd",
+                    type = "String",
+                    value = "data od kiedy ma liczyc",
+                    example = "2016-01-02",
+                    required = true,
+                    defaultValue = "GBP")
             @RequestParam String dataOd,
+            @ApiParam(
+                    name = "dataDo",
+                    type = "String",
+                    value = "Data do kiedy ma liczyc",
+                    example = "2016-01-06",
+                    required = true,
+                    defaultValue = "GBP")
             @RequestParam String dataDo) {
         return ResponseEntity.ok(tabelkiSerwis.dajTabelke(waluta, dataOd, dataDo));
     }
